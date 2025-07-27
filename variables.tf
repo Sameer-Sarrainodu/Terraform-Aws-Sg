807a8617a25f3215b15c69361c400e4ec004569e
b27user12 <b27user12@rpsconsulting.in>
2025-07-27 20:48:36 +0530

    sggroupconfirmed

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project" {
+    type = string
+}
+variable "sg_description" {
+    type = string
+  
+}
+variable "vpc_id" {
+    type = string
+}
+
+variable "sg_tags" {
+    type=map(string)
+    default ={}
+
+  
+}
+variable "environment" {
+    type = string
+  
+}
+variable "sg_name" {
+    type=string
+  
+}
